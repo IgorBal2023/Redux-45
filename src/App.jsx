5c68ad19181d68016b4b47cfc74ea880d1e563e0
IgorBal2023 <protuberanec2023@gmail.com>
2023-12-27 19:12:26 +0200

    first-47

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import './App.css'
-import { Provider } from 'react-redux'
-import { store } from './store/store'
-import { Settings } from './store/settings'
 
 function App() {
 
   return (
     <>
-    <Provider store={store}>
-<Settings/>
-    </Provider>
     </>
   )
 }
